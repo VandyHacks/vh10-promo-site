dbb23b6a23156cdf72f82899e3d98b7d15950968
Zi Nean Teoh <zinean00@gmail.com>
2023-09-10 16:48:16 -0500

    add svg components

---
+++ src/components/schedule/light-bulb.svelte
@@ -0,0 +1,91 @@
+<svg
+  width="182"
+  height="197"
+  viewBox="0 0 182 197"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <rect
+    x="63.5508"
+    y="160.189"
+    width="55.1084"
+    height="10.3827"
+    rx="4"
+    fill="#D9D9D9"
+  />
+  <rect
+    x="63.5508"
+    y="172.968"
+    width="55.1084"
+    height="10.3827"
+    rx="4"
+    fill="#D9D9D9"
+  />
+  <rect
+    x="63.5508"
+    y="185.747"
+    width="55.1084"
+    height="10.3827"
+    rx="4"
+    fill="#D9D9D9"
+  />
+  <g filter="url(#filter0_di_723_4)">
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M117.861 141.297C135.468 131.674 147.412 112.984 147.412 91.5036C147.412 60.1859 122.023 34.7979 90.7058 34.7979C59.388 34.7979 34 60.1859 34 91.5036C34 112.657 45.5828 131.105 62.7522 140.852V153.794C62.7522 156.003 64.5431 157.794 66.7522 157.794H113.861C116.07 157.794 117.861 156.003 117.861 153.794V141.297Z"
+      fill="white"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_di_723_4"
+      x="0"
+      y="0.797852"
+      width="181.412"
+      height="190.996"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="17" />
+      <feComposite in2="hardAlpha" operator="out" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.866667 0 0 0 0 0.578139 0 0 0 0 0.0180556 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_723_4"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow_723_4"
+        result="shape"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="12.5" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.866667 0 0 0 0 0.578139 0 0 0 0 0.0180556 0 0 0 1 0"
+      />
+      <feBlend mode="normal" in2="shape" result="effect2_innerShadow_723_4" />
+    </filter>
+  </defs>
+</svg>
