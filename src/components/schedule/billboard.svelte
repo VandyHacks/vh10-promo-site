c6c5390e1253f30a5b83e05af5d11c3832cdbd28
Zi Nean Teoh <zinean00@gmail.com>
2023-09-19 00:29:32 -0500

    implement monospaced rows on individual events

--- src/components/schedule/billboard.svelte
+++ src/components/schedule/billboard.svelte
@@ -19,19 +19,49 @@
     <BillboardDesktop />
   {/if}
 
+  <!-- Billboard -->
   <div class="container">
     <div class="title" class:title-mobile={matches}>Schedule</div>
-    {#if currentPage == 1}
-      <!-- TODO: render page 1 content here -->
-      {#each page1Data.events as event}
-        <div>{event.name} | {event.time}</div>
-      {/each}
-    {:else if currentPage == 2}
-      <!-- TODO: render page 2 content here -->
-      {#each page2Data.events as event}
-        <div>{event.name} | {event.time}</div>
-      {/each}
-    {/if}
+
+    <div class="content">
+      {#if currentPage == 1}
+        <!-- TODO: render page 1 content here -->
+        {#each page1Data.events as event}
+          {@const eventName = event.name}
+          {@const eventTime = event.time}
+          <div class="row">
+            <div class="row-start">
+              {#each eventName as char}
+                <span class="row-item">{char}</span>
+              {/each}
+            </div>
+            <div class="row-end">
+              {#each eventTime as char}
+                <span class="row-item">{char}</span>
+              {/each}
+            </div>
+          </div>
+        {/each}
+      {:else if currentPage == 2}
+        <!-- TODO: render page 2 content here -->
+        {#each page2Data.events as event}
+          {@const eventName = event.name}
+          {@const eventTime = event.time}
+          <div class="row">
+            <div class="row-start">
+              {#each eventName as char}
+                <span class="row-item">{char}</span>
+              {/each}
+            </div>
+            <div class="row-end">
+              {#each eventTime as char}
+                <span class="row-item">{char}</span>
+              {/each}
+            </div>
+          </div>
+        {/each}
+      {/if}
+    </div>
   </div>
 
   <!-- light bulbs -->
@@ -42,23 +72,21 @@
 </MediaQuery>
 
 <style>
+  /* Container */
   .container {
     position: absolute;
     width: 90%;
     height: 100%;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
-    flex-direction: column;
-    pointer-events: none;
   }
 
+  /* Title */
   .title {
     font-size: 3rem;
     font-weight: 500;
-    margin-bottom: 2rem;
-    position: absolute;
-    top: 10%;
   }
 
   .title-mobile {
@@ -66,6 +94,27 @@
     top: 13%;
   }
 
+  /* Schedule content and monospace rows */
+  .content {
+    width: 100%;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+    margin: 0.5rem 0;
+    font-family: monospace;
+    font-size: x-large;
+  }
+  .row-item {
+    margin: 0 1px;
+    padding: 0 2px;
+    border: 0.01em solid rgb(211, 211, 211);
+    background-color: white;
+  }
+
+  /* Lightbulb container */
   .bulb-container {
     position: absolute;
     width: 20%;
