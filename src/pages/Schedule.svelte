a52b2c870e1b8c9306449ac48292d78ada700a21
Zi Nean Teoh <zinean00@gmail.com>
2023-09-19 23:58:14 -0500

    implement background for mobile

--- src/pages/Schedule.svelte
+++ src/pages/Schedule.svelte
@@ -1,47 +1,59 @@
 <script>
-  import CloudLeft from "../components/schedule/cloud-left.svelte";
-  import CloudRight from "../components/schedule/cloud-right.svelte";
+  import MediaQuery from "../MediaQuery.svelte";
+
+  import MountainLeft from "../components/schedule/mountain-left.svelte";
+  import MountainRight from "../components/schedule/mountain-right.svelte";
   import Billboard from "../components/schedule/billboard.svelte";
   import BuildingLeft from "../components/schedule/building-left.svelte";
   import BuildingRight from "../components/schedule/building-right.svelte";
   import BuildingBottom from "../components/schedule/building-bottom.svelte";
   import BuildingTop from "../components/schedule/building-top.svelte";
   import RoadTop from "../components/schedule/road-top.svelte";
+  import MountainMobile from "../components/schedule/mountain-mobile.svelte";
 
   let currentPage = 1;
 </script>
 
-<div class="container">
-  <!-- background -->
-  <div class="road-top">
-    <RoadTop />
-  </div>
-  <div class="cloud-left">
-    <CloudLeft />
-  </div>
-  <div class="cloud-right">
-    <CloudRight />
-  </div>
-  <div class="building-left">
-    <BuildingLeft />
-  </div>
-  <div class="building-right">
-    <BuildingRight />
-  </div>
-  <div class="building-bottom">
-    <BuildingBottom />
-  </div>
-  <div class="building-top">
-    <BuildingTop />
-  </div>
+<MediaQuery query="(max-width: 768px)" let:matches>
+  <div class="container">
+    {#if !matches}
+      <!-- desktop background -->
+      <div class="mountain-left">
+        <MountainLeft />
+      </div>
+      <div class="mountain-right">
+        <MountainRight />
+      </div>
+      <div class="road-top">
+        <RoadTop />
+      </div>
+      <div class="building-left">
+        <BuildingLeft />
+      </div>
+      <div class="building-right">
+        <BuildingRight />
+      </div>
+      <div class="building-bottom">
+        <BuildingBottom />
+      </div>
+      <div class="building-top">
+        <BuildingTop />
+      </div>
+    {:else}
+      <!-- mobile background -->
+      <div class="mountain-mobile">
+        <MountainMobile />
+      </div>
+    {/if}
 
-  <!-- billboard -->
-  <div class="center">
-    <div class="billboard">
-      <Billboard bind:currentPage />
+    <!-- billboard -->
+    <div class="center">
+      <div class="billboard">
+        <Billboard bind:currentPage />
+      </div>
     </div>
   </div>
-</div>
+</MediaQuery>
 
 <style>
   .container {
@@ -83,17 +95,21 @@
     z-index: 1;
     left: -5vw;
   }
-  .cloud-left {
+  .mountain-left {
     position: absolute;
     width: 50vw;
     left: 0px;
     z-index: 1;
   }
-  .cloud-right {
+  .mountain-right {
     position: absolute;
     width: 10vw;
     right: 0px;
     z-index: 1;
+  }
+  .mountain-mobile {
+    position: absolute;
+    width: 100vw;
   }
   .building-left {
     position: absolute;
