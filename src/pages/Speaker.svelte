d6d75dee03ef2247156b1805aeb0fa9e8170aa5f
Zi Nean Teoh <zinean00@gmail.com>
2023-09-21 15:03:28 -0500

    refactor Speaker

--- src/pages/Speaker.svelte
+++ src/pages/Speaker.svelte
@@ -1,91 +1,86 @@
 <script>
+  import MediaQuery from "../MediaQuery.svelte";
+
   import SpeakerCircle from "../components/speaker/speakerCircle.svelte";
   import Scene from "../components/speaker/scene.svelte";
 </script>
 
-<div style="position:relative;">
-  <div class="speakerAndText" style="position:absolute;">
-    <div class="speakercircle">
-      <SpeakerCircle />
-    </div>
-
-    <div class="text">
-      <div class="title">Speaker Series</div>
-      <div class="name">Co-President Alex Oh</div>
-      <div class="description">
-        Alex Oh is a junior at Vanderbilt University and is a really cool dude.
-        Like a really cool dude. He is also one of the Co-Presidents of
-        VandyHacks X. This is another sentence talking about how awesome he is.
+<MediaQuery query="(max-width: 768px)" let:matches>
+  <div class="container" class:container-mobile={matches}>
+    <div class="speakerAndText">
+      <div class="speakercircle">
+        <SpeakerCircle />
       </div>
-      <!-- implement logic for changing names and description -->
+
+      <div class="text">
+        <div class="title">Speaker Series</div>
+        <div class="name">Co-President Alex Oh</div>
+        <div>
+          Alex Oh is a junior at Vanderbilt University and is a really cool
+          dude. Like a really cool dude. He is also one of the Co-Presidents of
+          VandyHacks X. This is another sentence talking about how awesome he
+          is.
+        </div>
+        <!-- implement logic for changing names and description -->
+      </div>
+    </div>
+
+    <div class="scene">
+      <Scene />
     </div>
   </div>
-
-  <div class="scene" style="position:absolute;">
-    <Scene />
-  </div>
-</div>
+</MediaQuery>
 
 <style>
+  .container {
+    display: block;
+    position: relative;
+    width: 100vw;
+    height: 150vh;
+  }
+  .container-mobile {
+    height: 60vh;
+  }
+  .speakerAndText {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 50vh;
+  }
   .speakercircle {
-    width: 40%;
+    width: 30%;
     float: left;
     padding-left: 7vw;
-    z-index: 4;
-    position: relative;
-    top: -15em;
   }
   .text {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    height: 100%;
     width: 40%;
-    float: right;
-    font: Inter;
     font-size: 1.5em;
     font-weight: 200;
     padding-right: 7vw;
-    z-index: 4;
+    color: white;
   }
   .title {
-    display: flex;
-    justify-content: left;
     text-shadow: 0 0 7px #fff;
-    color: white;
-    font-size: 3em;
+    font-size: 2.5em;
     font-weight: 700;
-    position: relative;
-    top: -2em;
-    font: Inter;
-    z-index: 4;
   }
 
   .name {
-    display: flex;
-    justify-content: left;
     font-size: 1.4em;
     font-weight: 700;
-    position: relative;
-    top: -3em;
-    color: white;
-    font: Inter;
-    z-index: 4;
-  }
-
-  .description {
-    display: flex;
-    justify-content: left;
-    font-size: 1em;
-    position: relative;
-    top: -3em;
-    color: white;
-    font: Inter;
-    z-index: 4;
   }
 
   .scene {
+    position: absolute;
     width: 100%;
     z-index: 4;
   }
 
-  @media (max-width: 450px) {
+  @media (max-width: 768px) {
     .scene {
       display: none;
     }
