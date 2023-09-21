6ec584774d8bd819be960908480104c20a13a0e2
Adaline <90662211+AdalineL@users.noreply.github.com>
2023-09-21 04:00:42 -0500

    added some missing components

--- src/pages/Schedule.svelte
+++ src/pages/Schedule.svelte
@@ -1,19 +1,14 @@
 <script>
-  import CloudLeft from "../components/schedule/cloud-left.svelte";
-  import CloudRight from "../components/schedule/cloud-right.svelte";
   import Billboard from "../components/schedule/billboard.svelte";
   import LightBulb from "../components/schedule/light-bulb.svelte";
+  import Scene from "../components/schedule/scene.svelte";
 
   let currentPage = 1;
 </script>
 
 <div class="container">
-  <!-- cloud -->
-  <div class="cloud-left">
-    <CloudLeft />
-  </div>
-  <div class="cloud-right">
-    <CloudRight />
+  <div class="scene" style="position:absolute;">
+    <Scene />
   </div>
 
   <!-- billboard -->
@@ -35,24 +30,13 @@
     position: relative;
     width: 100vw;
     height: 100vh;
+    margin-bottom: 20em;
 
     /* for development purpose */
     background-color: #140f2e;
     border: 1px solid black;
   }
 
-  .cloud-left {
-    position: absolute;
-    width: 50vw;
-    left: 0px;
-    z-index: 1;
-  }
-  .cloud-right {
-    position: absolute;
-    width: 10vw;
-    right: 0px;
-    z-index: 1;
-  }
   .center {
     position: absolute;
     width: 100vw;
@@ -60,13 +44,13 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    z-index: 2;
+    z-index: 4;
   }
   .billboard {
     position: relative;
     margin: 0 auto;
     display: flex;
-    width: 900px;
+    width: 90vw;
     height: 600px;
     object-fit: fill;
     justify-content: center;
@@ -83,9 +67,22 @@
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    z-index: -1;
+    z-index: 3;
 
     /* for development purpose */
     /* background-color: greenyellow; */
+  }
+
+  .scene {
+    width: 100%;
+    z-index: 2;
+    position: absolute;
+    top: -15em;
+  }
+
+  @media (max-width: 450px) {
+    .scene {
+      display: none;
+    }
   }
 </style>
