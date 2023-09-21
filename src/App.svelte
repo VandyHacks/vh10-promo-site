7bb348f470c113e36083d2abc3d57cb0306662e3
Adaline <90662211+AdalineL@users.noreply.github.com>
2023-09-21 02:50:16 -0500

    initial speaker and FAQ

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,19 @@
 <script>
   import Home from "./pages/Home.svelte";
+  import Speaker from "./pages/Speaker.svelte";
+  import FAQ from "./pages/FAQ.svelte";
 </script>
 
-<Home />
+<div class="all">
+  <Home />
+  <Speaker />
+  <FAQ />
+</div>
 
 <style lang="postcss" global>
+  .all {
+    display: grid;
+    grid-template-columns: repeat(1, auto);
+    grid-template-rows: repeat(3, 1fr);
+  }
 </style>
---
+++ src/components/speaker/speakerCircle.svelte
@@ -0,0 +1,66 @@
+<svg
+  width="100%"
+  height="676"
+  viewBox="0 0 626 626"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g filter="url(#filter0_f_930_2)">
+    <circle
+      cx="312.5"
+      cy="314.5"
+      r="218.5"
+      stroke="#FF509F"
+      stroke-width="30"
+    />
+  </g>
+  <circle cx="312.5" cy="314.5" r="218.5" stroke="white" stroke-width="13" />
+  <g filter="url(#filter1_f_930_2)">
+    <circle cx="313" cy="313" r="263" stroke="#AC59FF" stroke-width="30" />
+  </g>
+  <circle cx="313" cy="313" r="263" stroke="white" stroke-width="13" />
+  <defs>
+    <filter
+      id="filter0_f_930_2"
+      x="44"
+      y="46"
+      width="537"
+      height="537"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="BackgroundImageFix"
+        result="shape"
+      />
+      <feGaussianBlur
+        stdDeviation="17.5"
+        result="effect1_foregroundBlur_930_2"
+      />
+    </filter>
+    <filter
+      id="filter1_f_930_2"
+      x="0"
+      y="0"
+      width="626"
+      height="626"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="BackgroundImageFix"
+        result="shape"
+      />
+      <feGaussianBlur
+        stdDeviation="17.5"
+        result="effect1_foregroundBlur_930_2"
+      />
+    </filter>
+  </defs>
+</svg>
