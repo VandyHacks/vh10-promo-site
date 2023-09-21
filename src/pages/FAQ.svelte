89d26019372f3226e01018ac6fb11bf68e45d61e
Zi Nean Teoh <zinean00@gmail.com>
2023-09-21 15:02:48 -0500

    refactor FAQ

--- src/pages/FAQ.svelte
+++ src/pages/FAQ.svelte
@@ -2,33 +2,39 @@
   import Boards from "../components/FAQ/boards.svelte";
 </script>
 
-<div>
+<div class="container">
   <div class="title">FAQ</div>
 
   <div class="boards">
+    <!-- TODO: split this into three components: leftSigns, mainBoard, and rightSigns -->
     <Boards />
   </div>
 </div>
 
 <style>
+  .container {
+    display: block;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+  }
   .title {
     display: flex;
-    justify-content: left;
+    justify-content: center;
+    align-items: center;
     text-shadow: 0 0 7px #fff;
     color: white;
+    width: 30vw;
+    padding-left: 10px;
+    padding-top: 5vh;
     font-size: 3em;
     font-weight: 700;
-    position: relative;
-    left: 1em;
-    font: Inter;
-    z-index: 4;
   }
-
   .boards {
     width: 100%;
+    height: 90%;
     display: flex;
     justify-content: center;
-    position: relative;
-    z-index: 4;
+    align-items: center;
   }
 </style>
