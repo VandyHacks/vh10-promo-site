46c0367cfb3f03306d1654f18a483e37ba221acf
Alexa Reid <alexa.a.reid@gmail.com>
2023-09-27 23:51:22 -0500

    mobile responsiveness

--- src/pages/Footer.svelte
+++ src/pages/Footer.svelte
@@ -1,93 +1,128 @@
 <script>
+  import MediaQuery from "../MediaQuery.svelte";
   import FooterApartment from "../components/footer/footerApartment.svelte";
 </script>
 
-<footer>
-  <div id="design">
-    <div id="footerApartment">
-      <FooterApartment />
+<MediaQuery query="(max-width: 768px)" let:matches>
+  <div class="container" class:container-mobile={matches}>
+    <div class="design">
+      <div class="footerApartment">
+        <FooterApartment />
+        <a class="apply" href="https://apply.vandyhacks.org">Apply Now</a>
+      </div>
     </div>
-    <a id="apply" href="https://apply.vandyhacks.org">Apply Now</a>
+    <div class="links">
+      <div class="block">
+        <a href="https://apply.vandyhacks.org/">Judge Portal</a>
+        <br /><br />
+        <a href="https://www.asurion.com/">Asurion</a>
+        <br /><br />
+        <a href="https://xy.vandyhacks.org/">Crossyear</a>
+      </div>
+      <div class="break"></div>
+      <div class="block">
+        <a href="https://www.instagram.com/vandyhacks/">Instagram</a>
+        <br /><br />
+        <a href="https://www.youtube.com/@vandyhacks3426">Youtube</a>
+        <br /><br />
+        <a href="https://github.com/VandyHacks">Github</a>
+        <br /><br />
+        <a href="https://discord.gg/GVsHeNR9j6">Discord</a>
+        <br /><br />
+        <a href="https://x.com/vandyhacks?s=21&t=gRhN7Dcc5Ae-f-c2JKc0ig"
+          >X (Twitter)</a
+        >
+        <br /><br />
+        <a href="https://www.facebook.com/vandyhacks/">Facebook</a>
+      </div>
+      <div class="block">
+        <p>Made with &lt3 by the VandyHacks team</p>
+      </div>
+    </div>
   </div>
-  <div class="links">
-    <div class="block">
-      <a href="https://apply.vandyhacks.org/">Judge Portal</a>
-      <br /><br />
-      <a href="https://www.asurion.com/">Asurion</a>
-      <br /><br />
-      <a href="https://xy.vandyhacks.org/">Crossyear</a>
-    </div>
-    <div class="block">
-      <a href="https://www.instagram.com/vandyhacks/">Instagram</a>
-      <br /><br />
-      <a href="https://www.youtube.com/@vandyhacks3426">Youtube</a>
-      <br /><br />
-      <a href="https://github.com/VandyHacks">Github</a>
-      <br /><br />
-      <a href="https://discord.gg/GVsHeNR9j6">Discord</a>
-      <br /><br />
-      <a href="https://x.com/vandyhacks?s=21&t=gRhN7Dcc5Ae-f-c2JKc0ig"
-        >X (Twitter)</a
-      >
-      <br /><br />
-      <a href="https://www.facebook.com/vandyhacks/">Facebook</a>
-    </div>
-    <div class="block">
-      <p>Made with &lt3 by the VandyHacks team</p>
-    </div>
-  </div>
-</footer>
+</MediaQuery>
 
 <style>
-  footer {
-    width: 100vw;
-    height: 500px;
-    background-color: #0e0e1a;
-    text-align: left;
-    font-size: 20px;
-    position: relative;
-    bottom: 0px;
-  }
   a {
     color: white;
     text-decoration: none;
   }
   .links {
     position: absolute;
-    right: 0;
     display: flex;
+    right: 10vw;
+    padding: 10px;
+  
+  }
+  .container {
+    width: 100vw;
+    height: 80vh; 
+    background-color: #0e0e1a;
+    text-align: left;
+    font-size: 1.5em;
+    position: relative;
+    bottom: 0px;
+    margin: 0;
   }
   .block {
-    margin-top: 45px;
     width: 10vw;
     padding: 5vw;
     color: white;
+
   }
-  #footerApartment {
+
+  .footerApartment {
     position: absolute;
-    display: inline-block;
-    top: 30px;
+    width: 290px;
+    height:434px;
+    top: 10vh;
   }
 
-  #apply {
+  .apply {
     background-color: #1d1743;
-    top: 180px;
-    left: 168px;
     padding: 34px;
-    position: relative;
     border-radius: 10px;
+    bottom: 290px;
+    left: 20vh;
+    position: relative;
   }
 
-  #apply:hover {
+  /* .apply:hover {
     text-shadow: 0 0 20px rgba(255, 56, 131, 1);
-  }
+  } */
 
   @media (max-width: 865px) {
-    #footerApartment {
-      display: none;
+    .links{
+      flex-direction: column;
+      position: static;
+    
     }
-    #apply {
-      display: none;
+    .break{
+      height: 2px;
+      background-color: white;
     }
+    .block{
+      width: 70vw;
+      text-align: left;
+      padding-left: 0;
+    }
+    .container{
+      font-size: 1.3em;
+      height: 170vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    
+    .design{
+      width: 40vw;
+      height: 40vh;
+      z-index: 1;
+      order: 2;
+      position: relative;
+      bottom: 30px;
+    }
+
   }
+
 </style>
