7f46a44fd7e8c88b0bf4b6f1424facb8ff43ea05
Adaline <90662211+AdalineL@users.noreply.github.com>
2023-09-28 06:49:14 -0500

    about

--- src/pages/About-Desktop.svelte
+++ src/pages/About-Desktop.svelte
@@ -1,133 +1,133 @@
 <script>
-	import AboutLeftMountains from "../components/aboutLeftMountains.svelte";
-	import AboutRightMountains from "../components/aboutRightMountains.svelte";
-	import AboutMountain_1 from "../components/about/about_mountain_1.svelte";
+  import AboutLeftMountains from "../components/aboutLeftMountains.svelte";
+  import AboutRightMountains from "../components/aboutRightMountains.svelte";
+  import AboutMountain_1 from "../components/about/about_mountain_1.svelte";
 </script>
 
 <div style="position: relative; display: flex; justify-content: space-around;">
-	<div class="text-container">
-		<div class="about_title">About VH X</div>
-		<div class="about_desc">
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-			Lorem Ipsum has been the industry's standard dummy text ever since the
-			1500s, when an unknown printer took a galley of type and scrambled it to
-			make a type specimen book.<br /><br />It has survived not only five
-			centuries, but also the leap into electronic typesetting, remaining
-			essentially unchanged. It was popularised in the 1960s with the release of
-			<br /><br />Letraset sheets containing Lorem Ipsum passages, and more
-			recently with desktop publishing software like Aldus PageMaker including
-			versions of Lorem Ipsum.
-		</div>
-	</div>
+  <div class="text-container">
+    <div class="about_title">About VH X</div>
+    <div class="about_desc">
+      VandyHacks, Vanderbilt's premier student-run hackathon, proudly stands as
+      the largest collegiate hackathon in the South. Celebrating our 10th
+      edition, VandyHacks X invites global participants to immerse in 24
+      electrifying hours of innovation, from October 28-29.
+      <br /><br />
+      Set against a vibrant neon theme, this fully in-person event beckons hackers
+      to craft groundbreaking projects, attend illuminating workshops, explore career
+      opportunities, and vie for coveted prizes.
+      <br /><br />
+      Join us, and let's light up the future together!
+    </div>
+  </div>
 
-	<div class="right-container">
-		<div class="main-img-container mountain-img-container">
-			<img src="/img/groupphoto.png" alt="group photo" />
-			<div class="mountain1_svg">
-				<AboutMountain_1 />
-			</div>
-		</div>
-		<div class="miniature-container">
-			<div class="mountain-img-container">
-				<img
-					src="/img/miniature1.png"
-					class="miniature-img"
-					alt="miniature 1"
-				/>
-				<div class="leftmountain_svg">
-					<AboutLeftMountains />
-				</div>
-			</div>
-			<div class="mountain-img-container">
-				<img
-					src="/img/miniature2.png"
-					class="miniature-img"
-					alt="miniature 2"
-				/>
-				<div class="rightmountain_svg">
-					<AboutRightMountains />
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="right-container">
+    <div class="main-img-container mountain-img-container">
+      <img src="/img/groupphoto.png" alt="group photo" />
+      <div class="mountain1_svg">
+        <AboutMountain_1 />
+      </div>
+    </div>
+    <div class="miniature-container">
+      <div class="mountain-img-container">
+        <img
+          src="/img/miniature1.png"
+          class="miniature-img"
+          alt="miniature 1"
+        />
+        <div class="leftmountain_svg">
+          <AboutLeftMountains />
+        </div>
+      </div>
+      <div class="mountain-img-container">
+        <img
+          src="/img/miniature2.png"
+          class="miniature-img"
+          alt="miniature 2"
+        />
+        <div class="rightmountain_svg">
+          <AboutRightMountains />
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
-	.text-container {
-		flex: 1;
-		max-width: calc(100% / 3);
-		padding: 20px;
-	}
+  .text-container {
+    flex: 1;
+    max-width: calc(100% / 3);
+    padding: 20px;
+  }
 
-	.about_title {
-		color: #fff;
-		text-shadow: 0px 0px 20px rgba(255, 255, 255, 0.73);
-		font-family: Inter;
-		font-size: 80px;
-		font-style: normal;
-		font-weight: 700;
-		line-height: normal;
-	}
-	.about_desc {
-		width: 504px;
-		color: white;
-		font-size: 20px;
-		font-family: Inter;
-		font-weight: 400;
-		word-wrap: break-word;
-	}
+  .about_title {
+    color: #fff;
+    text-shadow: 0 0 7px #fff;
+    font-family: Inter;
+    font-size: 2.5em;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  .about_desc {
+    width: 40vw;
+    color: white;
+    font-size: 1.5em;
+    font-weight: 200;
+    word-wrap: break-word;
+  }
 
-	/* Container styles */
-	.mountain-img-container {
-		position: relative;
-		height: min-content;
-	}
+  /* Container styles */
+  .mountain-img-container {
+    position: relative;
+    height: min-content;
+  }
 
-	.main-img-container img {
-		width: 550px;
-		object-fit: cover;
-		z-index: 0;
-	}
+  .main-img-container img {
+    width: 550px;
+    object-fit: cover;
+    z-index: 0;
+  }
 
-	.mountain1_svg {
-		position: absolute;
-		z-index: 1;
-		bottom: -25px;
-		width: 200px;
-		left: -85px;
-	}
+  .mountain1_svg {
+    position: absolute;
+    z-index: 1;
+    bottom: -25px;
+    width: 200px;
+    left: -85px;
+  }
 
-	.leftmountain_svg {
-		position: absolute;
-		z-index: 1;
-		bottom: -40px;
-		width: 145px;
-		left: -60px;
-	}
-	.rightmountain_svg {
-		position: absolute;
-		z-index: 1;
-		bottom: -17px;
-		width: 148px;
-		right: -52px;
-	}
+  .leftmountain_svg {
+    position: absolute;
+    z-index: 1;
+    bottom: -40px;
+    width: 145px;
+    left: -60px;
+  }
+  .rightmountain_svg {
+    position: absolute;
+    z-index: 1;
+    bottom: -17px;
+    width: 148px;
+    right: -52px;
+  }
 
-	.right-container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
+  .right-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-	.miniature-container {
-		margin-top: 50px;
-		display: flex;
-		justify-content: space-around;
-		width: 100%;
-	}
+  .miniature-container {
+    margin-top: 50px;
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+  }
 
-	.miniature-img {
-		width: 175px;
-		object-fit: cover;
-		border-radius: 50%;
-	}
+  .miniature-img {
+    width: 175px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
 </style>
--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,13 +1,13 @@
 <script>
-	import MediaQuery from "../MediaQuery.svelte";
-	import AboutDesktop from "./About-Desktop.svelte";
-	import AboutMobile from "./About-Mobile.svelte";
+  import MediaQuery from "../MediaQuery.svelte";
+  import AboutDesktop from "./About-Desktop.svelte";
+  import AboutMobile from "./About-Mobile.svelte";
 </script>
 
-<MediaQuery query="(max-width: 768px)" let:matches>
-	{#if matches}
-		<AboutMobile />
-	{:else}
-		<AboutDesktop />
-	{/if}
+<MediaQuery query="(max-width: 1300px)" let:matches>
+  {#if matches}
+    <AboutMobile />
+  {:else}
+    <AboutDesktop />
+  {/if}
 </MediaQuery>
