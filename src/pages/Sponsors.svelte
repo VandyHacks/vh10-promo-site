8835785344cfccc64700ca670b7802582c9441e9
shansitads <shansitads@gmail.com>
2023-10-02 00:41:02 -0500

    sponsor logos added

--- src/pages/Sponsors.svelte
+++ src/pages/Sponsors.svelte
@@ -4,23 +4,42 @@
   import Tree3 from "../components/sponsors/tree3.svelte";
   import Building1 from "../components/sponsors/building1.svelte";
   import Building2 from "../components/sponsors/building2.svelte";
+  import GlowingArrow from "../components/sponsors/glowingArrow.svelte";
   import Sponsor1 from "../components/sponsors/sponsor1.svelte";
   import Sponsor2 from "../components/sponsors/sponsor2.svelte";
+  import Sponsor3 from "../components/sponsors/sponsor3.svelte";
+  import Sponsor4 from "../components/sponsors/sponsor4.svelte";
+  import Sponsor5 from "../components/sponsors/sponsor5.svelte";
+  import Sponsor6 from "../components/sponsors/sponsor6.svelte";
+  import Sponsor7 from "../components/sponsors/sponsor7.svelte";
+  import Sponsor8 from "../components/sponsors/sponsor8.svelte";
+  import Sponsor9 from "../components/sponsors/sponsor9.svelte";
+  import Sponsor10 from "../components/sponsors/sponsor10.svelte";
+  import Sponsor11 from "../components/sponsors/sponsor11.svelte";
 </script>
 
 <div class="container">
   <div class="sponsorsHeader">Sponsors</div>
 
-  <div class="comingSoon">Coming Soon</div>
+  <!-- Include with asurion page: <div class="glowingArrow"><GlowingArrow /></div> -->
 
-  <div class="sponsor1">
-    <!-- TODO: split the title "Sponsors" from our actual sponsor -->
-    <!-- <Sponsor1 /> -->
+  <div class="sponsorsList">
+    <div class="sponsor"><Sponsor1 /></div>
+    <div class="sponsor"><Sponsor2 /></div>
+    <div class="sponsor"><Sponsor3 /></div>
+    <div class="sponsorHorizontal">
+      <div class="sponsor"><Sponsor4 /></div>
+      <div class="sponsor"><Sponsor10 /></div>
+    </div>
+    <div class="sponsor"><Sponsor5 /></div>
+    <div class="sponsor"><Sponsor6 /></div>
+    <div class="sponsor"><Sponsor8 /></div>
+    <div class="sponsorHorizontal">
+      <div class="sponsor"><Sponsor7 /></div>
+      <div class="sponsor"><Sponsor11 /></div>
+    </div>
+    <div class="sponsor"><Sponsor9 /></div>
   </div>
-
-  <!-- <div class="sponsor2">
-    <Sponsor2 />
-  </div> -->
 
   <div class="building1">
     <Building1 />
@@ -48,7 +67,7 @@
     display: block;
     position: relative;
     width: 100vw;
-    height: 170vh;
+    height: 240vh;
   }
   .sponsorsHeader {
     position: absolute;
@@ -59,41 +78,39 @@
     font-size: 3em;
     font-weight: 700;
     z-index: 1;
-    height: 100%;
-    margin-top: 30%;
-  }
-  .comingSoon {
-    position: absolute;
-    text-shadow: 0 0 7px #fff;
-    width: 100%;
-    text-align: center;
-    color: white;
-    font-size: 2em;
-    font-weight: 700;
-    z-index: 1;
-    height: 100%;
-    margin-top: 40%;
+    margin-top: 40vh;
   }
 
-  .sponsor1 {
+  .sponsorsList {
     position: absolute;
-    width: 40vw;
-    left: 30vw;
-    top: 30vh;
+    text-align: center;
     z-index: 1;
+    margin-top: 50vh;
+    width: 30vw;
+    left: 35vw;
   }
-  /* .sponsor2 {
-    position: absolute;
-    width: 40vw;
-    left: 30vw;
-    top: 40vh;
+
+  .sponsorsList > * {
     z-index: 1;
-  } */
+    width: 30vw;
+    margin-top: 5vh;
+    stroke: none;
+  }
+
+  .sponsorHorizontal {
+    display: flex;
+  }
+
+  .sponsorHorizontal > * {
+    flex: 1;
+    height: 100%;
+    margin: 5%;
+  }
 
   .building1 {
     position: absolute;
     width: 25vw;
-    top: 60vh;
+    top: 105vh;
     right: 1vw;
     z-index: 2;
   }
@@ -116,8 +133,8 @@
   .tree2 {
     position: absolute;
     width: 40vw;
-    left: 60vw;
-    top: 105vh;
+    left: 65vw;
+    top: 150vh;
     z-index: 3;
   }
 
