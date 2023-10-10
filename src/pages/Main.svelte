60e30ef3485a508c6ffdc30f43ed3167d94c8a8c
Zi Nean Teoh <zinean00@gmail.com>
2023-10-10 01:23:38 -0500

    set up routing

---
+++ src/pages/Main.svelte
@@ -0,0 +1,17 @@
+<script>
+  import About from "./About.svelte";
+  import Schedule from "./Schedule.svelte";
+  import Speaker from "./Speaker.svelte";
+  import FAQ from "./FAQ.svelte";
+  import Sponsors from "./Sponsors.svelte";
+  import Footer from "./Footer.svelte";
+  import HomeStar from "./HomeStar.svelte";
+</script>
+
+<HomeStar />
+<About />
+<Schedule />
+<Speaker />
+<FAQ />
+<Sponsors />
+<Footer />
