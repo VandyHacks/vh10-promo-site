5ca447a248fd0900e60d609abda1aa801810dc0f
Zi Nean Teoh <zinean00@gmail.com>
2023-10-11 19:24:32 -0500

    implement desktop questions and answers
    Co-authored-by: Alexa Reid <junolia@users.noreply.github.com>
    Co-authored-by: Lisa Liu <lisaliuu@users.noreply.github.com>

---
+++ src/components/FAQ/question.svelte
@@ -0,0 +1,33 @@
+<script>
+  import faq from "../../data/faq.json";
+
+  export let selectedIndex;
+  export let questionId;
+</script>
+
+<div
+  class="question-block"
+  on:click={() => {
+    selectedIndex = questionId;
+  }}
+  on:keypress={() => {
+    selectedIndex = questionId;
+  }}
+>
+  <!-- fallback if no question exists -->
+  {#if !faq[questionId]}
+    <div>Question not found</div>
+  {:else}
+    <div>Q: {faq[questionId].question}</div>
+  {/if}
+</div>
+
+<style>
+  .question-block {
+    /* height: 30%; */
+    vertical-align: middle;
+  }
+  .question-block:hover {
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/FAQ/questionBoard.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Question from "./question.svelte";
+
+  export let side;
+  export let selectedIndex;
+  export let boardId;
+</script>
+
+<div class={side}>
+  <Question bind:selectedIndex questionId={parseInt(boardId * 3) + 0} />
+  <Question bind:selectedIndex questionId={parseInt(boardId * 3) + 1} />
+  <Question bind:selectedIndex questionId={parseInt(boardId * 3) + 2} />
+</div>
+
+<style>
+  .left {
+    background-color: rgb(82, 56, 185);
+    height: 30%;
+    margin-bottom: 20px;
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: 25px;
+  }
+
+  .right {
+    background-color: rgb(82, 56, 185);
+    height: 30%;
+    margin-bottom: 20px;
+    border-top-left-radius: 25px;
+    border-bottom-left-radius: 25px;
+  }
+</style>
