adcacb27ee3f4527142f3705df9fabd5f866f3df
Zi Nean Teoh <zinean00@gmail.com>
2023-10-11 19:36:54 -0500

    import mobile faq
    Co-authored-by: Alexa Reid <junolia@users.noreply.github.com>
    Co-authored-by: Lisa Liu <lisaliuu@users.noreply.github.com>

---
+++ src/components/FAQ/MobileFAQBoard.svelte
@@ -0,0 +1,166 @@
+<svg
+  width="390"
+  height="647"
+  viewBox="0 0 390 647"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M48.3809 529.148H63.9297V639.003C63.9297 643.297 60.449 646.777 56.1553 646.777C51.8616 646.777 48.3809 643.297 48.3809 639.003V529.148Z"
+    fill="#201857"
+  />
+  <path
+    d="M77.1484 529.148H92.6973V639.003C92.6973 643.297 89.2166 646.777 84.9229 646.777C80.6292 646.777 77.1484 643.297 77.1484 639.003V529.148Z"
+    fill="#201857"
+  />
+  <path
+    d="M291.332 536.137H306.881V639.003C306.881 643.297 303.4 646.778 299.106 646.778C294.813 646.778 291.332 643.297 291.332 639.003V536.137Z"
+    fill="#201857"
+  />
+  <path
+    d="M320.1 536.137H335.648V639.003C335.648 643.297 332.168 646.778 327.874 646.778C323.58 646.778 320.1 643.297 320.1 639.003V536.137Z"
+    fill="#201857"
+  />
+  <g filter="url(#filter0_di_551_2660)">
+    <rect
+      x="37.5"
+      y="43.5"
+      width="313"
+      height="489.399"
+      rx="21.5"
+      stroke="white"
+      stroke-width="7"
+      shape-rendering="crispEdges"
+    />
+  </g>
+  <g filter="url(#filter1_di_551_2660)">
+    <rect
+      x="40"
+      y="47"
+      width="308"
+      height="482"
+      rx="18"
+      fill="#FF509F"
+      fill-opacity="0.1"
+      shape-rendering="crispEdges"
+    />
+    <rect
+      x="43.5"
+      y="50.5"
+      width="301"
+      height="475"
+      rx="14.5"
+      stroke="white"
+      stroke-width="7"
+      shape-rendering="crispEdges"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_di_551_2660"
+      x="-6"
+      y="0"
+      width="400"
+      height="576.399"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="20" />
+      <feComposite in2="hardAlpha" operator="out" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 1 0 0 0 0 0.227451 0 0 0 0 0.321569 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_551_2660"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow_551_2660"
+        result="shape"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="2.5" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 1 0 0 0 0 0.227451 0 0 0 0 0.321569 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="shape"
+        result="effect2_innerShadow_551_2660"
+      />
+    </filter>
+    <filter
+      id="filter1_di_551_2660"
+      x="0"
+      y="7"
+      width="388"
+      height="562"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="20" />
+      <feComposite in2="hardAlpha" operator="out" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.996078 0 0 0 0 0.796078 0 0 0 0 0.0941176 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow_551_2660"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow_551_2660"
+        result="shape"
+      />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset />
+      <feGaussianBlur stdDeviation="7.5" />
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0.996078 0 0 0 0 0.796078 0 0 0 0 0.0941176 0 0 0 1 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="shape"
+        result="effect2_innerShadow_551_2660"
+      />
+    </filter>
+  </defs>
+</svg>
