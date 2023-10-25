3094e637781667c62df30a4facd885ad8e6fb7e9
basicallyAlexOh <alexander.s.oh@vanderbilt.edu>
2023-10-25 02:03:33 -0500

    Fixed Schedule and Lightbulbs

--- src/components/schedule/billboard.svelte
+++ src/components/schedule/billboard.svelte
@@ -12,7 +12,7 @@
   let page2Data = schedule[1];
 
   // TODO: remove this when schedule is ready
-  const SCHEDULE_NOT_READY = true;
+  const SCHEDULE_NOT_READY = false;
 </script>
 
 <MediaQuery query="(max-width: 768px)" let:matches>
@@ -52,7 +52,7 @@
         {#each page2Data.events as event}
           {@const eventName = event.name}
           {@const eventTime = event.time}
-          <div class="row">
+          <div class:row={!matches} class:row-mobile={matches}>
             <div class="row-start">
               {#each eventName as char}
                 <span class="row-item">{char}</span>
@@ -95,6 +95,7 @@
   .title {
     font-size: 3rem;
     font-weight: 500;
+    padding-bottom: 2%;
   }
 
   .title-mobile {
@@ -118,15 +119,23 @@
     justify-content: space-between;
     width: 100%;
     margin: 0.5rem 0;
+    border-bottom: 0.01em solid rgb(211, 211, 211);
   }
   .row-mobile {
     margin: 0.5rem 0;
+    border-bottom: 0.01em solid rgb(211, 211, 211);
   }
   .row-item {
-    margin: 0 1px;
-    padding: 0 2px;
-    border: 0.01em solid rgb(211, 211, 211);
-    background-color: white;
+    margin: 0 0.25px;
+    /* padding: 0 2px; */
+    /* border: 0.01em solid rgb(211, 211, 211); */
+    /* background-color: white; */
+  }
+  .row-end {
+    margin-left: 5%;
+    white-space: nowrap;
+    text-align: right;
+    /* background-color: white; */
   }
 
   /* Lightbulb container */
@@ -141,8 +150,8 @@
     justify-content: space-between;
   }
   .bulb-container-mobile {
-    width: 40%;
-    height: 17%;
+    width: 30%;
+    /* height: 17%; */
     right: auto;
     top: -8%;
   }
